842ea1842f382ca000cdd8d84e45ab3cae382183
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-05-18 15:20:19 +0300

    Update maker.py

--- Maker/maker.py
+++ Maker/maker.py
@@ -72,15 +72,15 @@ async def auto_bot():
         except:
            pass
         try:
-          await user.join_chat("RQ_SF")
+          await user.join_chat("VEEVVW")
         except:
           pass
         try:
-          await user.join_chat("R3_QX")
+          await user.join_chat("VEEVVW")
         except:
           pass
         try:
-          await user.join_chat("R7_QX")
+          await user.join_chat("VEEVVW")
         except:
           pass
       except Exception as e:
@@ -219,8 +219,8 @@ async def stratmaked(client, message):
 async def source(c: Client, m):
   mostafa ="""
 ╭──── • ◈ • ────╮
-么 [ Source ](t.me/R7_QX).
-么  [ 𝗥͜𝗼͡𝗪͡𝗲͜𝗦 .](t.me/R7_OX).
+么 [ Source ](t.me/VEEVVW).
+么  [ 𝗥͜𝗼͡𝗪͡𝗲͜𝗦 .](t.me/NUNUU).
 ╰──── • ◈ • ────╯
 🚦 𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚂𝙾𝚄𝚁𝙲𝙴 𝙾𝙽 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 .
 """
